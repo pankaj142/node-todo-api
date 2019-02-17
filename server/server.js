2b5da91fbb44f280acb451e2ca6ce2b1349e2a04
pankaj142 <pankajagham1@gmail.com>
2019-02-18 00:16:10 +0530

    setup changes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('./db/mongoose');
 const {ObjectId} = require('mongodb');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 //Models
 var {Todos} = require('./models/todos');
